86db3b700a21d44a3ad1589fa9c26170bb469fc8
“TobiasBarbosa” <toha0006@stud.kea.dk>
2024-09-24 14:13:50 +0200

    Initial commit

---
+++ src/main/java/com/example/githubactionopgave/GitHubActionOpgaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionopgave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionOpgaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubActionOpgaveApplication.class, args);
+    }
+
+}
