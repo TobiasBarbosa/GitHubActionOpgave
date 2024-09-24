ff1c3033914a5f6c8a111cf30306a98546663892
“TobiasBarbosa” <toha0006@stud.kea.dk>
2024-09-24 14:38:30 +0200

    controller class and welcome.html

---
+++ src/main/java/com/example/githubactionopgave/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.example.githubactionopgave.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("welcome")
+@Controller
+public class WelcomeController {
+
+    @GetMapping("")
+    public String welcome(){
+        return "welcome";
+    }
+
+}
